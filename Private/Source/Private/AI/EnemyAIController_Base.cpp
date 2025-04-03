82281de65886620777844771bf26488102b5642f
GuSang2 <lsg0101231@naver.com>
2025-04-03 17:51:04 +0900

    2025.04.03

--- Private/Source/Private/AI/EnemyAIController_Base.cpp
+++ Private/Source/Private/AI/EnemyAIController_Base.cpp
@@ -2,13 +2,32 @@
 
 
 #include "EnemyAIController_Base.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 void AEnemyAIController_Base::StopBehaviorTree()
 {
-
+	if (BrainComponent)
+	{
+		BrainComponent->StopLogic(TEXT("BT Stop"));
+	}
 }
 
 void AEnemyAIController_Base::OnPossess(APawn* InPawn)
 {
 	Super::OnPossess(InPawn);
+
+	if (BehaviorTree)
+	{
+		RunBehaviorTree(BehaviorTree);
+
+		UBlackboardComponent* BlackboardComp = GetBlackboardComponent();
+		if (BlackboardComp)
+		{
+			UWorld* World = GetWorld();
+			APlayerController* PlayerController = World->GetFirstPlayerController();
+			APawn* PlayerPawn = PlayerController->GetPawn();
+			BlackboardComp->SetValueAsObject("TargetPlayer", PlayerPawn);
+		}
+	}
 }
